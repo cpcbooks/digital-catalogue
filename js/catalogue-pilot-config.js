06ab75ccb6055b0633908312f6ecaa9a1f2d3e12
cpcbooks <cpcbooksindia@gmail.com>
2026-09-20 16:34:49 +0530

    Configure Supabase catalogue pilot publishable key

--- js/catalogue-pilot-config.js
+++ js/catalogue-pilot-config.js
@@ -1,4 +1,4 @@
 window.CPC_CATALOGUE_CONFIG = Object.freeze({
   supabaseUrl: "https://ysaxagxortpxyifyaydx.supabase.co",
-  publishableKey: ""
+  publishableKey: "sb_publishable_D6Vf_6ifdOcF_e1gop5CHA_cef4wmee"
 });
